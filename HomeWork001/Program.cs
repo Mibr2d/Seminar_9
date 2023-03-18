80be747e6d516c4215d6223bbebca71f400dc661
mibr2d <hodakov7419@gmail.com>
2023-03-18 11:09:30 +0300

    V1

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все 
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+int Inputint(string message)
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+int M = Inputint("Введите число M");
+int N = Inputint("Введите число N");
+Console.WriteLine($"N={N}, M={M}");
+
+
+    if (M > N)
+        for (int i = N; i <= M; i++)
+        {
+            if(i%2==0)
+            Console.Write($" {i}");
+        }
+    else
+        for (int i = M; i <= N; i++)
+        {
+            if(i%2==0)
+            Console.Write($" {i}");
+        }
+
+
+
+
